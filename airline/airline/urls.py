21dd322682596b48b77711d519a860aa02f780d9
evezsick <ariadna.evellen08@aluno.ifce.edu.br>
2023-03-23 16:53:57 -0300

    continuação

--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("flights/", include("flights.urls")),
+    path("<int:flight_id>", views.flight, name="flight")
 ]
